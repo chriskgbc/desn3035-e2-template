a294553f73b732fb3d8106008c222c0ac320d4df
chriskgbc <148381481+chriskgbc@users.noreply.github.com>
2024-09-10 04:58:59 +0000

    Default client dete

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 "use client"
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        if (window !== undefined) {
+            setIsClient(true)
+        }
+    }, [])
+    
+    return (isClient ? <>
+        <h1>Hello, Next.js!</h1>
+    </> : <></>)
 }
